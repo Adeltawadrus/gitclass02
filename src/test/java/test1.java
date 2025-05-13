6d54b45c6762756d63c45531566da2df355427e6
Adeltawadrus <adeltawadrus@gmail.com>
2025-05-13 12:08:58 -0400

    person2 commit

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -1,6 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("this is project version 5");
-// done with version 5
+        System.out.println("this is project version 6 made by person2");
+// done with version six
     }
 }
